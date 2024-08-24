2a65c70e608aa52fb7027acf4c21f2253c730011
hyeon6895@naver.com <hyeon6895@naver.com>
2024-08-24 22:50:40 +0900

    0824 영화&리뷰 프로젝트 -리포지티 리뷰 무비에 영화 목록 페이지에 영화, 영화 이미지, 리뷰 수, 평점 출력하는 JPQL로 쿼리 작성
    -영화 정보 생성 테스트 완료(페이징 처리)
    -리뷰 생성 테스트 완료

--- src/main/java/com/example/mreview/repository/MovieRepository.java
+++ src/main/java/com/example/mreview/repository/MovieRepository.java
@@ -1,7 +1,21 @@
 package com.example.mreview.repository;
 
 import com.example.mreview.entity.Movie;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+    // 영화 목록 페이지에 영화, 영화 이미지, 리뷰 수, 평점 출력
+    // coalesce(r.grade, 0): 평점이 null인 경우 0으로 대체
+//    @Query("select m, mi, avg(coalesce(r.grade, 0)), count(distinct r) from Movie m"
+//            + " left outer join MovieImage mi on mi.movie = m"
+//            + " left outer join Review r on r.movie = m group by m") // 먼저 입력된 이미지 번호와 연결
+    @Query("select m, i, count(r) from Movie m"
+            + " left join MovieImage i on i.movie = m"
+            + " and i.inum = (select max(i2.inum) from MovieImage i2 where i2.movie = m)"
+            + " left outer join Review r on r.movie = m group by m") // 나중에 추가된 이미지 번호와 연결
+    Page<Object[]> getListPage(Pageable pageable);
 }
--- src/test/java/com/example/mreview/repository/MovieRepositoryTests.java
+++ src/test/java/com/example/mreview/repository/MovieRepositoryTests.java
@@ -5,9 +5,14 @@ import com.example.mreview.entity.MovieImage;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Arrays;
+import java.util.Objects;
 import java.util.UUID;
 import java.util.stream.IntStream;
 
@@ -25,15 +30,17 @@ public class MovieRepositoryTests {
     @Test
     public void insertMovies() {
 
-        IntStream.rangeClosed(1,100).forEach(i -> {
+        IntStream.rangeClosed(1, 100).forEach(i -> {
 
-            Movie movie = Movie.builder().title("Movie...." + i).build();
+            Movie movie = Movie.builder()
+                    .title("Movie...." + i)
+                    .build();
 
             System.out.println("-----------------------------------------");
 
             movieRepository.save(movie); // Movie 객체 우선 save()
 
-            int count = (int)(Math.random() * 5) + 1; // 1, 2, 3, 4, 5
+            int count = (int)(Math.random()*5) + 1; // 1, 2, 3, 4, 5
 
             for (int j = 0; j < count; j++) {
                 MovieImage movieImage = MovieImage.builder()
@@ -47,5 +54,17 @@ public class MovieRepositoryTests {
 
             System.out.println("========================================");
         });
+    }
+
+    @Test
+    public void testListPage() {
+
+        PageRequest pageRequest = PageRequest.of(0, 10, Sort.by(Sort.Direction.DESC, "mno"));
+
+        Page<Object[]> result = movieRepository.getListPage((pageRequest));
+
+        for (Object[] objects : result.getContent()) {
+            System.out.println(Arrays.toString(objects));
+        }
     }
 }
