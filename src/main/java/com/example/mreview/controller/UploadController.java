24b47a94a8eac0f281018ffbf1a43951fee97195
hyeon6895@naver.com <hyeon6895@naver.com>
2024-08-30 23:06:49 +0900

    0830 영화&리뷰 프로젝트 -컨트롤러 업로드 파일 확장자 검사, 디렉터리 생성

--- src/main/java/com/example/mreview/controller/UploadController.java
+++ src/main/java/com/example/mreview/controller/UploadController.java
@@ -6,21 +6,66 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.UUID;
+
 @RestController
 @Log4j2
 public class UploadController {
 
-    @Value("${com.example.mreview.path}")
+    @Value("${com.example.mreview.upload.path}")
     private String uploadPath;
 
     @PostMapping("/uploadAjax")
     public void uploadFile(MultipartFile[] uploadFiles) {
 
         for (MultipartFile uploadFile: uploadFiles) {
+
+            // 파일 검사 과정
+            if (uploadFile.getContentType().startsWith("image") == false) {
+                log.warn("이 파일은 이미지 타입이 아닙니다.");
+                return;
+            }
+
             String originalName = uploadFile.getOriginalFilename();
+
             String fileName = originalName.substring(originalName.lastIndexOf("\\") + 1);
 
             log.info("fileName: {}", fileName);
+
+            // 날짜 폴더 생성
+            String folderPath = makeFolder();
+
+            String uuid = UUID.randomUUID().toString();
+
+            String saveName = uploadPath + File.separator + folderPath + File.separator + uuid + "_" + fileName;
+
+            Path savePath = Paths.get(saveName);
+
+            try {
+                uploadFile.transferTo(savePath);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
+    }
+
+    private String makeFolder() {
+
+        String str = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd"));
+
+        String folderPath = str.replace("/", File.separator);
+
+        File uploadPathFolder = new File(uploadPath, folderPath);
+
+        if (uploadPathFolder.exists() == false) {
+            uploadPathFolder.mkdirs(); // mkdirs(): 디렉토리 생성
+        }
+        return folderPath;
     }
 }
