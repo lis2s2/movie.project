e78c78ac962d6b64f59dcac517b0126c7050f0e4
hyeon6895@naver.com <hyeon6895@naver.com>
2024-09-02 22:10:42 +0900

    0902 영화&리뷰 프로젝트 -전송하는 MIME 타입이 달라져야 하는 문제는 java.nio.file 패키지의 Files.probeContentType() 이용 - 파일 데이터 처리는 스프링의 FileCopyUtils 이용

--- src/main/java/com/example/mreview/controller/UploadController.java
+++ src/main/java/com/example/mreview/controller/UploadController.java
@@ -3,14 +3,19 @@ package com.example.mreview.controller;
 import com.example.mreview.dto.UploadResultDTO;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
@@ -75,5 +80,33 @@ public class UploadController {
             uploadPathFolder.mkdirs(); // mkdirs(): 디렉토리 생성
         }
         return folderPath;
+    }
+
+    @GetMapping("/display")
+    public ResponseEntity<byte[]> getFile(String fileName) {
+
+        ResponseEntity<byte[]> result = null;
+
+        try {
+            String srcFileName = URLDecoder.decode(fileName, "UTF-8");
+
+            log.info("fileName: " + srcFileName);
+
+            File file = new File(uploadPath + File.separator + srcFileName);
+
+            log.info("file: " + file);
+
+            HttpHeaders header = new HttpHeaders();
+
+            // MIME타입 처리
+            header.add("Content Type", Files.probeContentType(file.toPath()));
+            
+            // 파일 데이터 처리
+            result = new ResponseEntity<>(FileCopyUtils.copyToByteArray(file), header, HttpStatus.OK);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return result;
     }
 }
